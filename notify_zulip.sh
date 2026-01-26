bd2f2b2b5e324e69f87a8b78c1290e83b287aeda
André Barbosa <andre.d.b@gmail.com>
2026-01-26 18:20:21 +0000

    feat: add support for TEST status notifications in Zulip

--- notify_zulip.sh
+++ notify_zulip.sh
@@ -21,6 +21,9 @@ fi
 if [ "$STATUS" == "SUCCESS" ]; then
     ICON=":check_mark:"
     TITLE="Backup Sucesso"
+elif [ "$STATUS" == "TEST" ]; then
+    ICON=":loudspeaker:"
+    TITLE="Teste de Notificação"
 else
     ICON=":stop_sign:"
     TITLE="Backup Falha"
